3d18320d0b905c16962bc7b99fb05027d368fa0e
A-taro <e.yamatsuta49@gmail.com>
2018-04-03 06:59:47 +0900

    Brush up MemoApp

--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -82,7 +82,7 @@ const styles = StyleSheet.create({
     fontSize: 15,
   },
   editButton: {
-    top: 75,
+    top: 68,
   },
 });
 
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -38,6 +38,7 @@ class SignupScreen extends React.Component {
           autoCapitalize="none"
           autoCorrect={false}
           placeholder="Email Address"
+          underlineColorAndroid="transparent"
         />
         <TextInput
           style={styles.input}
@@ -47,6 +48,7 @@ class SignupScreen extends React.Component {
           autoCorrect={false}
           placeholder="Password"
           secureTextEntry
+          underlineColorAndroid="transparent"
         />
         <TouchableHighlight style={styles.button} onPress={this.handleSubmit.bind(this)} underlayColor="#C70F66">
           <Text style={styles.buttonTitle}>登録する</Text>
